e8a388f615b3a9ef7754eb541a121f4dd6ab7157
ishan <ishan.hcl.tab1@gmail.com>
2021-08-20 00:34:17 +0530

    Add top slider (responsive design remaining)

--- src/components/TopSlider/TopSlider.js
+++ src/components/TopSlider/TopSlider.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./TopSlider.module.css";
 import image from "../../assets/images/slider.png";
+import Slider from './Slider/Slider'
 function TopSlider() {
   return (
     <div
-      className={classes.TopSlider}
-      style={{ backgroundImage: `url(${image})` }}
-    ></div>
+      className={classes.TopSlider}   
+    ><Slider /></div>
   );
 }
 
